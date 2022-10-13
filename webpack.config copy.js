0b83c0cfe3234cca0f0cff47bea6f61271f638df
Dazhia Blassingame <dayblassingame@gmail.com>
2022-10-13 14:18:00 -0400

    webpack, react, babel, package.json setup complete

---
+++ webpack.config copy.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(_dirName, './src'),
+    devtool: "eval-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                }]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',]
+            },
+            {
+                test: /\.(svg|png|jpg|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                        plugins: ['@babel/plugin-macros']
+                    }
+                },
+                ],
+              },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, './src'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        port: 0888,
+        hot: true,
+    },
+};
